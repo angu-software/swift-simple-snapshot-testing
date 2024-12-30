d477111375441ab461302c33f94deb83fca1f7bd
Andreas Günther <andreas.guenther@adidas.com>
2024-12-30 15:10:51 +0100

    Convert test cases from XCTest to SwiftTesting

---
+++ Tests/SimpleSnapshotTestingTests/SnapshotImageRendererTests.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+import Testing
+
+@testable import SimpleSnapshotTesting
+
+@MainActor
+@Suite
+struct SnapshotImageRendererTests {
+
+    @Test
+    func should_create_an_image_from_a_view() throws {
+        #expect(makeSnapshotImage(view: Text("Hello World")) != nil)
+    }
+
+    @Test
+    func should_create_image_with_views_size() throws {
+        let view = Text("Hello World")
+            .frame(width: 100, height: 100)
+
+        let image = try #require(makeSnapshotImage(view: view))
+
+        #expect(image.size == CGSize(width: 100, height: 100))
+    }
+
+    @Test
+    func should_create_image_with_default_scale() throws {
+        let image = try #require(makeSnapshotImage(view: Text("Hello World")))
+
+        #expect(image.scale == 1)
+    }
+
+    private func makeSnapshotImage<SwiftUIView: SwiftUI.View>(view: SwiftUIView) -> SnapshotImage? {
+        return SnapshotImageRenderer.makeImage(view: view)
+    }
+}

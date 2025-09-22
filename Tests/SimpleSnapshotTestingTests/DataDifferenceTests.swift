5f4ed1b862c2038cacb0475a16e15dd539f887f9
angu-software <>
2025-09-22 12:53:55 +0200

    feat(Precision-comparison): Add Data comparison taking precision into account

--- Tests/SimpleSnapshotTestingTests/DataDifferenceTests.swift
+++ Tests/SimpleSnapshotTestingTests/DataDifferenceTests.swift
@@ -39,3 +39,50 @@ func dataDifferenceCount(lhs: Data, rhs: Data) -> Int {
 
     return differingBytes + lengthDifference
 }
+
+struct DataComparisonTests {
+
+    @Test
+    func whenDataIsEqual_itReturnsTrue() {
+        let data1 = Data([1])
+        let data2 = Data([1])
+
+        #expect(compare(data1, data2, precision: 1.0))
+    }
+
+    @Test
+    func whenDataIsDiffersWithinPrecision_itReturnsTrue() {
+        let data1 = Data([1, 2])
+        let data2 = Data([1, 1])
+
+        #expect(compare(data1, data2, precision: 0.5))
+    }
+
+    @Test
+    func whenDataIsDiffersBeyondPrecision_itReturnsFalse() {
+        let data1 = Data([1, 2, 2, 2])
+        let data2 = Data([1, 1, 1, 1])
+
+        #expect(compare(data1, data2, precision: 0.5) == false)
+    }
+
+    @Test
+    func whenDataIsDifferentInSize_itReturnsFalse() {
+        let data1 = Data([1, 2, 2, 2])
+        let data2 = Data([1, 1])
+
+        #expect(compare(data1, data2, precision: 0.0) == false)
+    }
+}
+
+func compare(_ lhs: Data, _ rhs: Data, precision: Double) -> Bool {
+    guard lhs.count == rhs.count else {
+        return false
+    }
+
+    let byteCount = Double(lhs.count)
+    let diffCount = Double(dataDifferenceCount(lhs: lhs, rhs: rhs))
+    let matchingBytes = byteCount - diffCount
+
+    return matchingBytes / byteCount >= precision
+}

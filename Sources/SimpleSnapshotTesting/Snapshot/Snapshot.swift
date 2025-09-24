652c3be8ac5b4b2a92fe09c3875a1fb313419482
angu-software <>
2025-09-24 09:44:54 +0200

    refactor: Store NormalizedImageData in Snapshot instead of pngData and scale

--- Sources/SimpleSnapshotTesting/Snapshot/Snapshot.swift
+++ Sources/SimpleSnapshotTesting/Snapshot/Snapshot.swift
@@ -9,28 +9,43 @@ import SwiftUI
 
 typealias SnapshotImageData = Data
 
-struct Snapshot: Equatable {
+struct Snapshot {
 
-    let pngData: SnapshotImageData
-    let scale: CGFloat
+    let imageData: NormalizedImageData
     let filePath: SnapshotFilePath
-
-    @available(*, deprecated, message: "Use init(imageData: filePath:)")
-    init?(pngData: SnapshotImageData, scale: CGFloat, filePath: SnapshotFilePath) {
-        self.pngData = pngData
-        self.scale = scale
-        self.filePath = filePath
-    }
-}
-
-extension Snapshot {
 
     init(imageData: NormalizedImageData,
          filePath: SnapshotFilePath) {
 
-        self.pngData = imageData.data
-        self.scale = CGFloat(imageData.pixelBufferInfo.scale)
+        self.imageData = imageData
         self.filePath = filePath
+    }
+}
+
+extension Snapshot: Equatable {
+
+    var pngData: SnapshotImageData {
+        guard let data = NormalizedImageDataConverter().makePNGImageData(normalizedImageData: imageData)?.data else {
+            return Data()
+        }
+
+        return data
+    }
+
+    var scale: CGFloat {
+        return CGFloat(imageData.pixelBufferInfo.scale)
+    }
+
+    @available(*, deprecated, message: "Use init(imageData: filePath:)")
+    init?(pngData: SnapshotImageData, scale: CGFloat, filePath: SnapshotFilePath) {
+        guard let imageData = NormalizedImageDataConverter().makeNormalizedImageData(pngImageData: (pngData, Int(scale))) else {
+            self.init(imageData: NormalizedImageData(data: Data(), pixelBufferInfo: PixelBufferInfo(width: 0, height: 0, scale: Int(scale))),
+                      filePath: filePath)
+            return
+        }
+
+        self.init(imageData: imageData,
+                  filePath: filePath)
     }
 }
 

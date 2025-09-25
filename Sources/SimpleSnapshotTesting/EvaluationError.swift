b11f3d8cb34b4a2ab8202f8f6d58a27f34affde4
angu-software <>
2025-09-25 13:39:33 +0200

    feat: Provide meaningful error descriptions

--- Sources/SimpleSnapshotTesting/EvaluationError.swift
+++ Sources/SimpleSnapshotTesting/EvaluationError.swift
@@ -7,9 +7,19 @@
 
 import Foundation
 
-// Rename EvaluationResult
-public enum EvaluationError: Swift.Error {
+public enum EvaluationError: Swift.Error, CustomStringConvertible {
     case didRecordReferenceSnapshot
     case noReferenceSnapshotFound
     case snapshotNotMatchingReference
+
+    public var description: String {
+        switch self {
+        case .didRecordReferenceSnapshot:
+            return "Snapshot recorded! All future runs will compare against this reference."
+        case .noReferenceSnapshotFound:
+            return "No reference snapshot found! You might need to enable recording first."
+        case .snapshotNotMatchingReference:
+            return "Snapshot does not match its reference."
+        }
+    }
 }
